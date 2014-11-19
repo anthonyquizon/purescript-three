d10e5fe8839112c81b4f8951e767a630c1000174
Anthony Quizon <anthoq88@gmail.com>
2014-11-19 14:13:32 +1100

    motion example track mouse with frame count

--- examples/motion-squash-and-stretch/Main.purs
+++ examples/motion-squash-and-stretch/Main.purs
@@ -16,30 +16,78 @@ import           Graphics.Three.Types
 width    = 500
 height   = 500
 
+newtype Pos = Pos {
+          x :: Number
+        , y :: Number
+    }
+
+pos :: Number -> Number -> Pos
+pos x y = Pos {
+          x: x
+        , y: y
+    }
+
+instance showPos :: Show Pos where
+    show (Pos p) = 
+        "x: " ++ show p.x ++ ", y: " ++ show p.y
+
+newtype StateRef = StateRef {
+          frame :: Number
+        , pos   :: Pos
+        , prev  :: Pos
+    }
+
+instance showStateRef :: Show StateRef where
+    show (StateRef s) = 
+        "frame: " ++ show s.frame ++ "\n" ++
+        "pos: "   ++ show s.pos   ++ "\n" ++
+        "prev: "  ++ show s.prev  ++ "\n"
+
+stateRef :: Number -> Pos -> Pos -> StateRef
+stateRef f p pv = StateRef {
+          frame: f
+        , pos: p
+        , prev: pv
+    }
+
+
+initStateRef :: StateRef
+initStateRef = stateRef 0 nPos nPos
+    where
+        nPos = pos 0 0
 
 doAnimation :: forall eff. Eff (three :: Three | eff) Unit -> Eff (three :: Three | eff) Unit
 doAnimation animate = do
     animate
     requestAnimationFrame $ doAnimation animate
 
--- callback 
--- on mouse move
 
-onMouseMove :: forall eff. Number -> Number -> Eff (trace :: Trace, dom :: DOM | eff) Unit
-onMouseMove x y = do
-    print x
-    print y
+renderContext :: forall a eff. RefVal StateRef -> 
+                 Eff ( trace :: Trace, ref :: Ref, three :: Three | eff) Unit
+renderContext state = do
+    
+    modifyRef state $ \(StateRef s) -> stateRef (s.frame + 1) s.pos s.prev
+    s'@(StateRef s) <- readRef state
+    
+    print s'
+    return unit
+    {--Renderer.render c.renderer c.scene c.camera--}
+
+
+onMouseMove :: forall eff. RefVal StateRef -> Number -> Number -> Eff (ref :: Ref, trace :: Trace, dom :: DOM | eff) Unit
+onMouseMove state x y = do
+    modifyRef state $ \(StateRef s) -> stateRef s.frame s.prev (pos x y)
     return unit
 
-
 main = do
-    frame    <- newRef 0
+    state    <- newRef initStateRef
     renderer <- Renderer.createWebGL {antialias: true}
 
     Renderer.setSize renderer width height
     Renderer.appendToDomByID renderer "container"
 
-    mouseMove onMouseMove
+    mouseMove $ onMouseMove state
+    doAnimation $ renderContext state
 
 
 foreign import mouseMove """
