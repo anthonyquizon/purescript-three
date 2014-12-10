e22ff3660b2dbaab70c914da9710c7b3e59221cc
Anthony Quizon <anthoq88@gmail.com>
2014-12-10 15:12:29 +1100

    code cleanup

--- src/Graphics/Three/Object3D.purs
+++ src/Graphics/Three/Object3D.purs
@@ -37,11 +37,11 @@ createLine g m t = create g m $ show t
     where
         create = ffi ["geometry", "material", "lineType", ""] "new THREE.Line(geometry, material)"
 
-getPosition      :: forall a. (Object3D a) => a -> ThreeEff Vector3 
-getPosition      = ffi ["object", ""] "object.position"
+getPosition :: forall a. (Object3D a) => a -> ThreeEff Vector3 
+getPosition = ffi ["object", ""] "object.position"
 
-setPosition      :: forall a. (Object3D a) => a -> Number -> Number -> Number -> ThreeEff Unit
-setPosition      = fpi ["object", "x", "y", "z", ""] "object.position.set(x, y, z)" --TODO change to vector?
+setPosition :: forall a. (Object3D a) => a -> Number -> Number -> Number -> ThreeEff Unit
+setPosition = fpi ["object", "x", "y", "z", ""] "object.position.set(x, y, z)" --TODO change to vector?
 
 getRotationEuler :: forall a. (Object3D a) => a -> Number -> Number -> Number -> ThreeEff Euler
 getRotationEuler = ffi ["object", ""] "object.rotation"
@@ -49,8 +49,8 @@ getRotationEuler = ffi ["object", ""] "object.rotation"
 setRotationEuler :: forall a. (Object3D a) => a -> Number -> Number -> Number -> ThreeEff Unit
 setRotationEuler = fpi ["object", "x", "y", "z", ""] "object.rotation.set(x, y, z)" --TODO change to euler? Add order
 
-rotateIncrement  :: forall a. (Object3D a) => a -> Number -> Number -> Number -> ThreeEff Unit
-rotateIncrement  = fpi ["object", "x", "y", "z", ""] 
+rotateIncrement :: forall a. (Object3D a) => a -> Number -> Number -> Number -> ThreeEff Unit
+rotateIncrement = fpi ["object", "x", "y", "z", ""] 
     "object.rotation.x += x; object.rotation.y += y; object.rotation.z += z;"
 
 getGeometry :: forall a. (Renderable a) => a -> ThreeEff G.Geometry
