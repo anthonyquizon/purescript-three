a35320a2473bdc522cf9ac39a545b109964bd137
Anthony Quizon <anthoq88@gmail.com>
2015-01-29 19:08:03 +1100

    gulp live reload, plumber and refactor

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,62 @@
 'use strict'
 
-var gulp      	= require('gulp')
-  , purescript 	= require('gulp-purescript')
-  , connect     = require('gulp-connect')
-  , fs          = require('fs')
+var gulp       = require('gulp')
+  , purescript = require('gulp-purescript')
+  , connect    = require('gulp-connect')
+  , fs         = require('fs')
+  , plumber    = require('gulp-plumber')
 
 
-var bower_src = [
-    'bower_components/purescript-*/src/**/*.purs'
-];
+var OPTIONS = {
+    lib: {
+        src: [
+            'src/**/*.purs'
+          , 'bower_components/purescript-*/src/**/*.purs'
+        ],
+        watch: ['src/**/*.purs'],
+        dst: 'build/output',
+        options : {}
+    },
+    common: {
+        src: "examples/Common.purs"
+    }
+}
 
-var library = {
-    src: ['src/**/*.purs'],
-    dest: 'build/output',
-    options : {}
-};
 
-var common = "examples/Common.purs"
-
-function compile(src, dest, options) {
+function compile(src, dst, options) {
 	var psc = purescript.psc(options);
 
-	psc.on('error', function(e) {
-		console.error(e.message);
-		psc.end();
-	});
-
-	return gulp.src(src.concat(bower_src))
-               .pipe(psc)
-               .pipe(gulp.dest(dest));
+	gulp.src(src)
+        .pipe(plumber())
+        .pipe(psc)
+        .pipe(gulp.dest(dst))
+        .pipe(connect.reload())
 }
 
 function compileExample(name) {
 
-    var dir   = 'examples/' + name + '/'
-      , lib  = library.src.concat(common)
-      , src  = lib.concat(dir + '*.purs')
-      , dest = dir + 'output/'
+    var dir  = 'examples/' + name + '/'
+      , lib  = OPTIONS.lib.src
+      , dst  = dir + 'output/'
+      , src  = lib.concat(OPTIONS.common.src)
+                  .concat(dir + '*.purs')
       , opts = {
             output: 'main.js', 
             main: true
         }
 
-    compile(src, dest, opts);
+    compile(src, dst, opts);
 }
 
 function forExamples(callback) {
     var dirs = fs.readdirSync('examples');
     
     for (var i=0; i<dirs.length; i++) {
-        var name = dirs[i];
+        var name    = dirs[i]
+          , isDir   = fs.lstatSync("examples/" + name).isDirectory()
+          , isBower = dirs[i] == "bower_components"
 
-        if (!fs.lstatSync("examples/" + name).isDirectory() ||
-            dirs[i] == "bower_components")
-        {
+        if (!isDir || isBower) {
             continue;
         }
 
@@ -62,19 +65,20 @@ function forExamples(callback) {
 }
 
 gulp.task('dotPsci', function() {
-    var psci = purescript.dotPsci();
+    var psci = purescript.dotPsci()
 
-	psci.on('error', function(e) {
-		console.error(e.message);
-		psci.end();
-	});
-
-	return gulp.src(library.src.concat(bower_src))
-               .pipe(psci);
+	gulp.src(OPTIONS.lib.src)
+        .pipe(psci)
 });
 
 gulp.task('build', function() {
-    compile(library.src, library.dest, library.options);
+    var opts = OPTIONS.lib
+
+    compile(
+        opts.src, 
+        opts.dst, 
+        opts.options
+    );
 });
 
 gulp.task('examples', function() {
@@ -85,6 +89,7 @@ gulp.task('examples', function() {
 gulp.task('server', function() {
     connect.server({
         root: 'examples'
+      //, livereload: true
     });
 });
 
@@ -94,9 +99,9 @@ forExamples(function(name) {
     });
 });
 
-gulp.task('watch', ['build', 'examples'], function() {
-	gulp.watch(library.src, ['build', 'examples', 'dotPsci']);
-	gulp.watch(common, ['examples']);
+gulp.task('watch', ['build', 'examples', 'server'], function() {
+    gulp.watch(OPTIONS.lib.src, ['build', 'examples', 'dotPsci']);
+    gulp.watch(OPTIONS.common.src, ['examples']);
 
     forExamples(function(name) {
         var src = 'examples/' + name + '/**/*.purs'
