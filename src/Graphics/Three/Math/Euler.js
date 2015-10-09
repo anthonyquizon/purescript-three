3c0457af2d90605772c7e84029f5c2e6b49675ad
rintcius <rintcius@gmail.com>
2015-10-09 14:50:23 +0200

    fix Euler in same way as Vector

--- src/Graphics/Three/Math/Euler.js
+++ src/Graphics/Three/Math/Euler.js
@@ -3,7 +3,7 @@
 
 // module Graphics.Three.Math.Euler
 
-exports.createEulerFn = function(x) {
+exports.create = function(x) {
     return function(y) {
         return function(z) {
             return new THREE.Euler(x, y, z);
--- src/Graphics/Three/Math/Euler.purs
+++ src/Graphics/Three/Math/Euler.purs
@@ -11,11 +11,7 @@ foreign import data Euler :: *
 
 --TODO order
 
-foreign import createEulerFn :: forall eff. Fn3 Number Number Number Euler
-
-create :: forall eff. Number -> Number -> Number -> Euler
-create x y z = runFn3 createEulerFn x y z
-
+foreign import create :: forall eff. Number -> Number -> Number -> Euler
 
 instance showEuler :: Show Euler where
     show = ffi ["euler"] "'(' + euler.x + ', ' + euler.y + ', ' + euler.z + ')'"
